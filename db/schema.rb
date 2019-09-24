664ed5205b87d7d7b117ad32804539a56145f47c
Jostein Hamre <josteinhamre@gmail.com>
2019-09-24 12:35:40 +0200

    started, but realised that user needs more fields.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_085020) do
+ActiveRecord::Schema.define(version: 2019_09_24_092235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "availabilities", force: :cascade do |t|
+    t.date "from"
+    t.date "to"
+    t.bigint "bike_id"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bike_id"], name: "index_availabilities_on_bike_id"
+  end
 
   create_table "bikes", force: :cascade do |t|
     t.bigint "user_id"
@@ -42,5 +52,6 @@ ActiveRecord::Schema.define(version: 2019_09_24_085020) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "availabilities", "bikes"
   add_foreign_key "bikes", "users"
 end
