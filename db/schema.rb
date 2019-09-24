78d23327cff4a2182ed9015ecccaca9c90557068
Jaemin Park <jaemin0624@hotmail.com>
2019-09-24 13:40:35 +0200

    Dummy navbar added

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_085020) do
+ActiveRecord::Schema.define(version: 2019_09_24_093442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "availabilities", force: :cascade do |t|
+    t.date "from"
+    t.date "to"
+    t.bigint "bike_id"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bike_id"], name: "index_availabilities_on_bike_id"
+  end
 
   create_table "bikes", force: :cascade do |t|
     t.bigint "user_id"
@@ -30,6 +40,18 @@ ActiveRecord::Schema.define(version: 2019_09_24_085020) do
     t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.date "confirmed_at"
+    t.bigint "user_id"
+    t.bigint "bike_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bike_id"], name: "index_bookings_on_bike_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -42,5 +64,8 @@ ActiveRecord::Schema.define(version: 2019_09_24_085020) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "availabilities", "bikes"
   add_foreign_key "bikes", "users"
+  add_foreign_key "bookings", "bikes"
+  add_foreign_key "bookings", "users"
 end
