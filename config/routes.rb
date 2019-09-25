1c38a1110b6b859fc2214913504c238e8f0fd220
Jostein Hamre <josteinhamre@gmail.com>
2019-09-25 14:07:37 +0200

    created basic dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'bookings/new'
   devise_for :users
   root to: 'pages#home'
   resources :bikes, only: [ :show ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :bikes, only: [ :index, :show ]
-
+  resources :users, only: [:show]
 end
