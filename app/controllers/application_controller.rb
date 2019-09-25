f5b5c257a34f4f6041022014367a378d6dcd99a1
Jostein Hamre <josteinhamre@gmail.com>
2019-09-25 15:15:20 +0200

    Fixed seed and navbar.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,10 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
+
+  private
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
