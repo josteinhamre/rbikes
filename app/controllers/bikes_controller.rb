dc79732fe8cd5c4d9f7f2d7fb4d79da662f0a251
Agnieszka Stec <agnieszkaa.stec@gmail.com>
2019-09-27 13:22:34 +0200

    date query work

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,17 +1,24 @@
+require 'date'
+
 class BikesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show, :index]
   def index
-    if params[:query].present?
-      @bikes = Bike.where("%#{params[:query]}%")
-  # sql_query = "daterange(start_date, end_date, '[]') && daterange(:start_date, :end_date, '[]')"
+    if params[:start_date].present? #check for end_date at some point
+      start_date = Date.parse(params[:start_date])
+      end_date = Date.parse(params[:end_date])
+      @bikes = Bike.all
+      @bikes = @bikes.select do |bike|
+        bike.available_range?(start_date, end_date)
+      end
     else
-    @bikes = Bike.all
-  # end
+      @bikes = Bike.all
+    end
   end
-end
+
   def show
     @bike = Bike.find(params[:id])
   end
+
 
   def new
     @bike = Bike.new
