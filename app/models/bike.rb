56e8c50f27aabb8211a352381bc45292d20ba412
Jostein Hamre <josteinhamre@gmail.com>
2019-09-27 12:30:12 +0200

    Added method available_range to Bike.Rb

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -7,4 +7,15 @@ class Bike < ApplicationRecord
   has_many :bookings
   has_many :bikes
   mount_uploader :photo, PhotoUploader
+
+
+  def available_range(start_date, end_date)
+    bookings = self.bookings # Get bookings for this bike
+    booking_ranges = bookings.map { |b| (b.start_date..b.end_date).to_a }.flatten
+    query_range = (start_date..end_date).to_a
+    query_range.each do |date|
+      return false if booking_ranges.include?(date)
+    end
+    true
+  end
 end
